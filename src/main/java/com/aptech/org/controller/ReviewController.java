53ec0824a3cf185f794cd5b955f0b0913b9c07e6
davetheIII <davidodita11@gmail.com>
2022-10-06 10:56:10 +0100

    Completed Database

--- src/main/java/com/aptech/org/controller/ReviewController.java
+++ src/main/java/com/aptech/org/controller/ReviewController.java
@@ -1,47 +1,62 @@
 package com.aptech.org.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
-@Controller
 @RestController
 public class ReviewController {
-//localhost:8080/get
-	@ResponseBody
+	
+	@Value("${student.name: No Name}")
+	private String name;
+	
+	@Value("${student.email: No Email}")
+	private String email;
+	
+	//localhost:8080/getDetails
+	@GetMapping("/getDetails")
+	public String getStudentDetails() {
+		return "Student Details: " + name + " " + email;
+	}
+	
+	//localhost:8080/get
+	//@ResponseBody
 	@GetMapping("/get")
 	public String getStudent() {
-		return "GET Request was sent";
+		
+		return "GET Request Was Sent";
 		
 	}
 	
-	@ResponseBody
+	//localhost:8080/add
+	//@ResponseBody
 	@PostMapping("/add")
-	public String addStudent() {
-		return "ADD Request was sent";
+    public String addStudent() {
+		
+		return "POST Request Was Sent";
 		
 	}
-	
-	@ResponseBody
-	@PutMapping("/put")
-	public String UpdateStudent(@RequestParam("id") Long student_id,
-			@RequestParam("name") String stud_name) {
-		return "UPDATE Request was sent for id:" + student_id
-				+ "and name:"+ stud_name;
-		
-	}
-	
-	@ResponseBody
+    
+	//localhost:8080/update?id=50&name=Janet
+	//@ResponseBody
+	@PutMapping("/update")
+    public String updateStudent(@RequestParam("id") Long stud_id, @RequestParam("name") String stud_name) {
+    	
+    	return "Update Request Was Sent for id: " + stud_id + "and name: " + stud_name;
+    }
+    
+	//localhost:8080/delete
+	//@ResponseBody
 	@DeleteMapping("/delete/{id}")
-	public String deleteStudent(@PathVariable("id") long stud_id) {
-		return "DELETE Request was sent for id:" + stud_id;
-		
-	}
+    public String deleteStudent(@PathVariable("id") Long stud_id) {
+    	
+    	return "DELETE Request Was Sent for id: " + stud_id;
+    }
+	
 }
